a13ccc8fb39c62e39b3c193099191526b9c29b0a
Josh Glasson <josh.glasson@tesco.com>
2020-08-12 14:18:10 +0100

    Attempt to add 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,28 @@
+<style>
+	h3 {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>404</title>
+</svelte:head>
+
+<h1>404 - Page Not Found</h1>
+
+<h3>The page you are looking for is not here!</h3>
