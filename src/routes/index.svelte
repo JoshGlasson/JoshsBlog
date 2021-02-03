7702791b862699c2a956632ae909bfcc75ed6994
Josh Glasson <josh.glasson@tesco.com>
2021-02-03 15:06:10 +0000

    Add meta to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,15 @@
 </script>
 
 <svelte:head>
+    <title>Josh's Blog</title>
+	<meta name="description" content="My personal blog about Software Development, Sports, Music, and any more of my hobbies!">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@joshglasson" />
+	<meta name="twitter:creator" content="@joshglasson" />
+	<meta property="og:title" content="Josh's Blog">
+	<meta property="og:image" content="https://joshglasson.co.uk/logo.png">
+	<meta property="og:description" content="My personal blog about Software Development, Sports, Music, and any more of my hobbies!">
+	<meta property="og:url" content="https://joshglasson.co.uk/">
 	<!-- Google Adsense -->
 	<script data-ad-client="ca-pub-1501125542619304" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </svelte:head>
