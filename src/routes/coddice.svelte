06fc0fa0b2bbe97510e92b0f4e5867025f2620cd
Josh Glasson <josh.glasson@tesco.com>
2021-03-25 15:36:02 +0000

    CoD Random Drop picker

---
+++ src/routes/coddice.svelte
@@ -0,0 +1,60 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount( async () => { 
+		var pickButton=document.querySelector('.pickBtn');
+		var box=document.querySelector('.imageContainer');
+		var locations = ["Dam", "Military Base", "Quarry", "Airport", "Storage Town", "Superstore", "TV Station", "Stadium", "Lumber", "Boneyard", "Train Station", "Hospital", "Downtown", "Farmland", "Promenade West", "Promenade East", "Hills", "Park", "Shipwreck", "Port", "Prison"]
+
+		pickButton.addEventListener("click", randomDrop);
+
+		function randomDrop() {
+			var existingDropZone = document.querySelector('.dropLocation')
+
+			if (existingDropZone == null) {
+				var dropZone = document.createElement("h3");
+				dropZone.classList.add("dropLocation")
+				var randomDrop = randomLocation()
+				dropZone.innerText = randomDrop
+
+				var dropImage = document.createElement("img")
+				dropImage.src = "./images/coddice/" + randomDrop + ".png"
+
+				box.appendChild(dropZone)
+			} else {
+				existingDropZone.innerText = randomLocation()
+			}
+		}
+
+		function randomLocation() {
+			return locations[Math.floor(Math.random() * locations.length)]
+		}
+		
+	})
+</script>
+
+<style>
+	
+</style>
+
+<svelte:head>
+    <title>Josh's Blog</title>
+	<meta name="description" content="My personal blog about Software Development, Sports, Music, and any more of my hobbies!">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@joshglasson" />
+	<meta name="twitter:creator" content="@joshglasson" />
+	<meta property="og:title" content="Josh's Blog">
+	<meta property="og:image" content="https://joshglasson.co.uk/logo.png">
+	<meta property="og:description" content="My personal blog about Software Development, Sports, Music, and any more of my hobbies!">
+	<meta property="og:url" content="https://joshglasson.co.uk/">
+	<!-- Google Adsense -->
+	<script data-ad-client="ca-pub-1501125542619304" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</svelte:head>
+
+<h2>Call of Duty Warzone Drop Picker</h2>
+
+<div class="container">
+	<div class="imageContainer">
+	</div>
+	<button class="pickBtn">Random Drop Point</button>
+</div>
