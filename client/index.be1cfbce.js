278d895bfa09831fcfdc2a121ee2a7fa97603596
JoshGlasson <JoshGlasson@users.noreply.github.com>
2025-08-07 16:29:25 +0000

    deploy: f2672f89e390d907883823fc744a2b86f33d6da2

--- client/index.be1cfbce.js
+++
@@ -1,6 +0,0 @@
-/*!
- * reading-time
- * Copyright (c) Nicolas Gryman <ngryman@gmail.com>
- * MIT Licensed
- */
-function e(e){return" "===e||"\n"===e||"\r"===e||"\t"===e}var r=function(r,t){var o,n,i=0,u=0,d=r.length-1;for((t=t||{}).wordsPerMinute=t.wordsPerMinute||200,o=t.wordBound||e;o(r[u]);)u++;for(;o(r[d]);)d--;for(n=u;n<=d;){for(;n<=d&&!o(r[n]);n++);for(i++;n<=d&&o(r[n]);n++);}var a=i/t.wordsPerMinute,f=60*a*1e3;return{text:Math.ceil(a.toFixed(2))+" min read",minutes:a,time:f,words:i}},t=Object.freeze(Object.assign(Object.create(null),r,{default:r,__moduleExports:r}));export{t as i};
