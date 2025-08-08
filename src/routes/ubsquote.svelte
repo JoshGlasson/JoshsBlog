3a4a95c28eea02c7b7599e6fa4a569864a9b26e8
Josh Glasson <josh.glasson@tesco.com>
2025-08-08 16:10:03 +0100

    Remove adsense on quote page

--- src/routes/ubsquote.svelte
+++ src/routes/ubsquote.svelte
@@ -88,8 +88,6 @@
 	<meta property="og:image" content="https://joshglasson.co.uk/logo.png">
 	<meta property="og:description" content="My personal blog about Software Development, Sports, Music, and any more of my hobbies!">
 	<meta property="og:url" content="https://joshglasson.co.uk/">
-	<!-- Google Adsense -->
-	<script data-ad-client="ca-pub-1501125542619304" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </svelte:head>
 
 <head>
